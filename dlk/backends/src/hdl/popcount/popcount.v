dfc7dfd892b07fc5aa3389cb22f5742aa98337be
progrunner17 <g.sho1500@gmail.com>
2018-12-03 13:33:08 +0900

    reimplement Latency support

--- dlk/backends/src/hdl/popcount/popcount.v
+++ dlk/backends/src/hdl/popcount/popcount.v
@@ -1,16 +1,17 @@
 `default_nettype none
-
-// TODO:consider inseerting FFs.
-
-
-
-// TODO:    check if it is possible to synthesize multi-level for in generate block.
-//          if possible convert to utilize it.
+/*
+LATENCY: 0-2;
+NOTE:enableを定義してあるが、定義しない方がregisterの節約になると思われる。
+enableは、演算器全体を制御するわけではなく、そのクロックで入力されたデータの計算を許可する設計なので、validという方が近い。
+これにより、正しい入力が来るまでは、前のデータが消されない効果がある。
+多少if文が深いので、正しくgenerateされるか確証がない。
+*/
 
 module popcount32 #(parameter LATENCY = 0) (
-    input  wire        clk,
-    input  wire        en ,
-    input  wire [31:0] d  ,
+    input  wire        clk  ,
+    input  wire        rst_n,
+    input  wire        en   ,
+    input  wire [31:0] d    ,
     output wire [ 5:0] q
 );
 
@@ -26,19 +27,23 @@ module popcount32 #(parameter LATENCY = 0) (
 
     genvar i2;
     wire [2:0] tmp2[7:0];
+    wire tmp_en;
     generate
         if( LATENCY > 1 )begin
             reg [23:0] tmp2_reg;
+            reg tmp_en_reg = 0;
             for(i2 = 0; i2 < 8; i2 = i2 + 1)begin
                 always @(posedge clk ) begin
                     if(en)
                         tmp2_reg[i2*3+2:i2*3] = {1'b0,tmp1[i2*2]} + {1'b0,tmp1[i2*2+1]};
+                    tmp_en_reg <= en;
                 end
                 assign tmp2[i2] = tmp2_reg[i2*3+2:i2*3];
+                assign tmp_en = tmp_en_reg;
             end
 
         end else begin
-            
+            assign tmp_en = en;
             for(i2 = 0; i2 < 8; i2 = i2 + 1)begin
                 assign tmp2[i2] = {1'b0,tmp1[i2*2]} + {1'b0,tmp1[i2*2+1]};
             end
@@ -65,7 +70,7 @@ module popcount32 #(parameter LATENCY = 0) (
         if(LATENCY > 0)begin
             reg [5:0] out_reg = 0;
             always @(posedge clk ) begin
-                if(en)
+                if(tmp_en)
                     out_reg <= {1'b0,tmp4[0]} + {1'b0,tmp4[1]};
             end
             assign q = out_reg;
@@ -73,62 +78,88 @@ module popcount32 #(parameter LATENCY = 0) (
             assign q = {1'b0,tmp4[0]} + {1'b0,tmp4[1]};
         end
     endgenerate
+
+
 endmodule
 
 
 
-module popcount64 #(
-parameter LATENCY = 0
-    )(
-    input  wire        clk,
-    input  wire        en ,
-    input  wire [63:0] d  ,
+
+
+/*
+LATENCY: 0-3;
+NOTE:enableを定義してあるが、定義しない方がregisterの節約になると思われる。
+enableは、演算器全体を制御するわけではなく、そのクロックで入力されたデータの計算を許可する設計なので、validという方が近い。
+これにより、正しい入力が来るまでは、前のデータが消されない効果がある。
+多少if文が深いので、正しくgenerateされるか確証がない。
+*/
+module popcount64 #(parameter LATENCY = 0) (
+    input  wire        clk  ,
+    input  wire        rst_n,
+    input  wire        en   ,
+    input  wire [63:0] d    ,
     output wire [ 6:0] q
 );
 
 
-    localparam SUBLATENCY = (LATENCY <= 1)? 0:
-                            (LATENCY == 2)? 1:
-                            2;
+        localparam SUBLATENCY = (LATENCY <= 1)? 0:
+            (LATENCY == 2)? 1:
+            2;
 
 
-    wire [5:0] tmp[1:0];
-
-    popcount32 #(
-            .LATENCY(SUBLATENCY)
-        )u1 (
-        .clk(clk),
-        .en (en),
-        .d(d[63:32]),
-        .q(tmp[1]  )
+        wire [5:0] tmp[1:0];
+    popcount32 #(.LATENCY(SUBLATENCY)) u1 (
+        .clk(clk     ),
+        .en (en      ),
+        .d  (d[63:32]),
+        .q  (tmp[1]  )
     );
 
-    popcount32 #(
-            .LATENCY(SUBLATENCY)
-        ) u2 (
-        .clk(clk),
-        .en (en),
-        .d(d[31:0]),
-        .q(tmp[0] )
+    popcount32 #(.LATENCY(SUBLATENCY)) u2 (
+        .clk(clk    ),
+        .en (en     ),
+        .d  (d[31:0]),
+        .q  (tmp[0] )
     );
 
-    generate
-        if(LATENCY==0)begin
-            assign q = {1'b0,tmp[0]} + {1'b0,tmp[1]} ;
-        end
-        else begin
-            reg [6:0] q_reg = 0;
-            always @(posedge clk) begin
-                if(en) begin
-                    q_reg <=  {1'b0,tmp[0]} + {1'b0,tmp[1]} ;    
+
+        generate
+            if(LATENCY==0)begin
+                assign q = {1'b0,tmp[0]} + {1'b0,tmp[1]} ;
+            end else begin
+                wire en_wire;
+                if(LATENCY == 1)begin
+                    assign en_wire = en;
+                end else if(LATENCY == 2) begin
+                    reg shift_reg = 0;
+                    always @(posedge clk) begin
+                        if(~rst_n) begin
+                            shift_reg <= 0;
+                        end else begin
+                            shift_reg <= en;
+                        end
+                    end
+                    assign en_wire = shift_reg;
+                end else begin
+                    reg [1:0]shift_reg = 0;
+                    always @(posedge clk) begin
+                        if(~rst_n) begin
+                            shift_reg <= 0;
+                        end else begin
+                            shift_reg <= {shift_reg,en};
+                        end
+                    end
+                    assign en_wire = shift_reg[1];
                 end
+                reg [6:0] q_reg = 0;
+                always @(posedge clk) begin
+                    if(en_wire) begin
+                        q_reg <=  {1'b0,tmp[0]} + {1'b0,tmp[1]} ;
+                    end
+                end
+                assign q = q_reg;
             end
-            assign q = q_reg;
-        end
-    endgenerate
+        endgenerate
+    endmodule
 
-        
-
-endmodule
-
-`default_nettype wire
+        `default_nettype wire
